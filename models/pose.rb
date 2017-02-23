838c13a75c2287d460fa21e5d01883d424218990
Alice Loudon <alice_loudon@icloud.com>
2017-02-23 16:15:35 +0000

    Create 'Pose' class in pose.rb

---
+++ models/pose.rb
@@ -0,0 +1,17 @@
+# require_relative('../db/sql_runner.rb')
+
+class Pose
+
+  attr_reader :id
+  attr_accessor :name, :image, :hold_time, :effort_level
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @hold_time = options['hold_time']
+    @effort_level = options['effort_level']
+  end
+
+
+
+end

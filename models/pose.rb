e9ffc0437ab8026d0c78d4e8725e75a70d742b1d
Alice Loudon <alice_loudon@icloud.com>
2017-02-24 10:14:31 +0000

    Define methods 'save' and 'delete_all' in pose.rb

--- models/pose.rb
+++ models/pose.rb
@@ -13,6 +13,15 @@ class Pose
     @effort_level = options['effort_level']
   end
 
+  def save
+    sql = "INSERT INTO poses (name, image, hold_time, effort_level) VALUES ('#{@name}', '#{@image}', #{@hold_time}, #{@effort_level}) RETURNING *;"
+    pose = SqlRunner.run(sql).first
+    @id = pose['id'].to_i
+  end
 
+  def self.delete_all
+    sql = "DELETE FROM poses"
+    SqlRunner.run(sql)
+  end
 
 end

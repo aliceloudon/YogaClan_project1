2146a5b2bebdb2eaa6d90292d69fbe58b9736812
Alice Loudon <alice_loudon@icloud.com>
2017-02-25 10:42:49 +0000

    Create new views for sequences

--- controllers/sequences_controller.rb
+++ controllers/sequences_controller.rb
@@ -3,3 +3,39 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('../models/sequence.rb')
 
+get '/sequences' do
+  @sequences = Sequence.all
+  erb(:"sequences/index")
+end
+
+get '/sequences/new' do
+  erb(:"sequences/new")
+end
+
+post '/sequences' do
+  @sequence = Sequence.new(params)
+  @sequence.save
+  redirect to '/sequences'
+end
+
+get '/sequences/:id' do
+  @sequence = Sequence.find(params[:id])
+  erb(:"sequences/show")
+end
+
+get '/sequences/:id/edit' do
+  @sequence = Sequence.find(params[:id])
+  erb(:"sequences/edit")
+end
+
+post '/sequences/:id' do
+  sequence = Sequence.new(params)
+  sequence.update
+  redirect to "/sequences/#{sequence.id}"
+end
+
+post '/sequences/:id/delete' do
+  sequence = Sequence.find(params[:id])
+  sequence.delete
+  redirect to "/sequences"
+end
